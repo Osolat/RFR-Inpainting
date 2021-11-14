64acbc04bcadf39ce1b35d26e9c63bde2335690d
Benjamin Birch Hansen <201303577@post.au.dk>
2021-11-14 16:56:34 +0100

    small mods

--- model.py
+++ model.py
@@ -149,7 +149,7 @@ class RFRNetModel():
         real_B_feats = self.lossNet(real_B)
         fake_B_feats = self.lossNet(fake_B)
         comp_B_feats = self.lossNet(comp_B)
-        print(real_B_feats)
+        print(fake_B_feats.shape)
         tv_loss = self.TV_loss(comp_B * (1 - self.mask))
         style_loss = self.style_loss(real_B_feats, fake_B_feats) + self.style_loss(real_B_feats, comp_B_feats)
         preceptual_loss = self.preceptual_loss(real_B_feats, fake_B_feats) + self.preceptual_loss(real_B_feats,
